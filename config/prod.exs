1c37b06d1deff36a07679c52f6492b07f79c1b05
rafaishida <rafa.ishida@yahoo.com.br>
2018-03-01 11:18:34 -0300

    heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :tccv2, Tccv2.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "''https", host: "pacific-coast-53096.herokuapp.com/ ", port: 443],
+  url: [scheme: "https", host: "pacific-coast-53096.herokuapp.com/ ", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
   secrete_key_base: System.get_env("SECRET_KEY_BASE")
