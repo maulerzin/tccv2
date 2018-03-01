ce070c0ec78af38d1a300eb834335937c342d1d2
rafaishida <rafa.ishida@yahoo.com.br>
2018-03-01 11:11:40 -0300

    heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :tccv2, Tccv2.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "''https", host: "pacific-coast-53096.herokuapp.com/ ", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
-
+  secrete_key_base: System.get_env("SECRET_KEY_BASE")
 # Do not print debug messages in production
 config :logger, level: :info
 
@@ -58,4 +59,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
